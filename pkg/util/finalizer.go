98e877f7b781c57e5c671dfd6be6e2880b701d49
raffaelespazzoli <rspazzol@redhat.com>
2019-05-02 14:09:12 -0400

    fixed pointer

--- pkg/util/finalizer.go
+++ pkg/util/finalizer.go
@@ -5,13 +5,13 @@ import (
 )
 
 // IsBeingDeleted returns whether this object has been requested to be deleted
-func IsBeingDeleted(obj *metav1.Object) bool {
-	return !(*obj).GetDeletionTimestamp().IsZero()
+func IsBeingDeleted(obj metav1.Object) bool {
+	return !obj.GetDeletionTimestamp().IsZero()
 }
 
 // HasFinalizer returns whether this object has the passed finalizer
-func HasFinalizer(obj *metav1.Object, finalizer string) bool {
-	for _, fin := range (*obj).GetFinalizers() {
+func HasFinalizer(obj metav1.Object, finalizer string) bool {
+	for _, fin := range obj.GetFinalizers() {
 		if fin == finalizer {
 			return true
 		}
@@ -20,19 +20,19 @@ func HasFinalizer(obj *metav1.Object, finalizer string) bool {
 }
 
 // AddFinalizer adds the passed finalizer this object
-func AddFinalizer(obj *metav1.Object, finalizer string) {
+func AddFinalizer(obj metav1.Object, finalizer string) {
 	if !HasFinalizer(obj, finalizer) {
-		(*obj).SetFinalizers(append((*obj).GetFinalizers(), finalizer))
+		obj.SetFinalizers(append(obj.GetFinalizers(), finalizer))
 	}
 }
 
 // RemoveFinalizer removes the passed finalizer from object
-func RemoveFinalizer(obj *metav1.Object, finalizer string) {
-	for i, fin := range (*obj).GetFinalizers() {
+func RemoveFinalizer(obj metav1.Object, finalizer string) {
+	for i, fin := range obj.GetFinalizers() {
 		if fin == finalizer {
-			finalizers := (*obj).GetFinalizers()
+			finalizers := obj.GetFinalizers()
 			finalizers[i] = finalizers[len(finalizers)-1]
-			(*obj).SetFinalizers(finalizers[:len(finalizers)-1])
+			obj.SetFinalizers(finalizers[:len(finalizers)-1])
 			return
 		}
 	}
