20379bc09ed847ee4c1eae306d77f715e8c8e7f0
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2020-06-08 08:06:12 -0400

    Fix#22 (#23)
    * added the ability to validate resources
    before accepting them in the manager
    
    Signed-off-by: Raffaele Spazzoli <raffaele.spazzoli@gmail.com>
    
    * added validation for existance of the resource type
    
    Signed-off-by: Raffaele Spazzoli <raffaele.spazzoli@gmail.com>

--- pkg/util/templates.go
+++ pkg/util/templates.go
@@ -19,10 +19,13 @@ package util
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 
 	"text/template"
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/discovery"
+	"k8s.io/kubernetes/pkg/kubectl/cmd/util/openapi/validation"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/yaml"
 )
@@ -75,4 +78,38 @@ func ProcessTemplateArray(data interface{}, template *template.Template) ([]unst
 	}
 
 	return obj, err
+}
+
+// ValidateUnstructured validates the content of an unstructred against an openapi schema.
+// the schema is intented to be retrieved from a running instance of kubernetes, but other usages are possible.
+func ValidateUnstructured(obj *unstructured.Unstructured, validationSchema *validation.SchemaValidation) error {
+	bb, err := obj.MarshalJSON()
+	if err != nil {
+		log.Error(err, "unable to unmarshall", "unstrcutured", obj)
+		return err
+	}
+	err = validationSchema.ValidateBytes(bb)
+	if err != nil {
+		log.Error(err, "unable to valudate", "json doc", string(bb), "against schemas", validationSchema)
+		return err
+	}
+	return nil
+}
+
+//IsUnstructuredDefined checks whether the content of a unstructured is defined against the passed DiscoveryClient
+func IsUnstructuredDefined(obj *unstructured.Unstructured, discoveryClient *discovery.DiscoveryClient) error {
+	gvk := obj.GroupVersionKind()
+	resources, err := discoveryClient.ServerResourcesForGroupVersion(gvk.GroupVersion().String())
+	if err != nil {
+		log.Error(err, "unable to find resources for", "gvk", gvk)
+		return err
+	}
+	for _, resource := range resources.APIResources {
+		if resource.Kind == gvk.Kind {
+			return nil
+		}
+	}
+	err = errors.New("unable to find resource kind")
+	log.Error(err, "unable to find resource for ", "kind", gvk.Kind)
+	return err
 }
