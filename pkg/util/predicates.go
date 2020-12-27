21ca5e3853d7dd72fffb83fe6d3084a9ed4211ac
David J. M. Karlsen <david@davidkarlsen.com>
2020-12-27 18:50:38 +0100

    fix typo
    Signed-off-by: David J. M. Karlsen <david@davidkarlsen.com>

--- pkg/util/predicates.go
+++ pkg/util/predicates.go
@@ -23,7 +23,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-// ResourceGenerationOrFinalizerChangedPredicate this preducates will fire an update event when the spec of a resource is changed (controller by ResourceGeneration), or when the finalizers are changed
+// ResourceGenerationOrFinalizerChangedPredicate this predicate will fire an update event when the spec of a resource is changed (controller by ResourceGeneration), or when the finalizers are changed
 type ResourceGenerationOrFinalizerChangedPredicate struct {
 	predicate.Funcs
 }
